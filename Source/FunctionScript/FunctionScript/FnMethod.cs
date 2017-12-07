a437a64902fadbe83bfd295d3492c64a329c89e9
Jayden Tilbrook <jdosx@live.com>
2017-12-07 16:51:27 +1100

    + bunch'o' formatting and other changes to FnObject and FnMethod

--- Source/FunctionScript/FunctionScript/FnMethod.cs
+++ Source/FunctionScript/FunctionScript/FnMethod.cs
@@ -5,180 +5,163 @@ using System.Text;
 
 using System.Reflection;
 
-namespace FunctionScript
-{
-    public abstract class FnMethod<T> : FnObject<T>
-    {
-        #region Fields
-        /// <summary>
-        /// Stores the method arguments to be used when checking if the method
-        /// is cachable. Can be cleared using the ClearDataPostCache method
-        /// </summary>
-        private FnObject[] MethodArguments_CacheCheck;
-        /// <summary>
-        /// Stores the data types for the arguments which this method will contain when filled.
-        /// Is only populated if an FnMethod is constructed blankly.
-        /// </summary>
-        internal Type[] ArgumentTypes;
-        /// <summary>
-        /// The local variable which stores the values which are used by the CompileFlags Property
-        /// </summary>
-        internal FnScriptResources.CompileFlags[] CompileFlags;
+namespace FunctionScript {
+  /// <summary>
+  /// Reflection tag for identifying if an FnObject is a function argument in an FnMethod.
+  /// </summary>
+  [System.AttributeUsage(AttributeTargets.Field)]
+  public class FnArg : System.Attribute { }
 
-        /// <summary>
-        /// Stores the field information for the FnMethod's arguments.
-        /// Can be cleared using the ClearDataPostCache method
-        /// </summary>
-        private FieldInfo[] ArgsInfo;
+  public abstract class FnMethod<T> : FnObject<T> {
+    #region Fields
+    /// <summary>
+    /// Stores the method arguments to be used when checking if the method
+    /// is cachable. Can be cleared using the ClearDataPostCache method
+    /// </summary>
+    private FnObject[] MethodArguments_CacheCheck;
+    /// <summary>
+    /// Stores the data types for the arguments which this method will contain when filled.
+    /// Is only populated if an FnMethod is constructed blankly.
+    /// </summary>
+    internal Type[] ArgumentTypes;
+    /// <summary>
+    /// The local variable which stores the values which are used by the CompileFlags Property
+    /// </summary>
+    internal FnScriptResources.CompileFlags[] CompileFlags;
 
-        internal FnVariable<Boolean> IsPreExecute;
+    /// <summary>
+    /// Stores the field information for the FnMethod's arguments.
+    /// Can be cleared using the ClearDataPostCache method
+    /// </summary>
+    private FieldInfo[] ArgsInfo;
 
-        protected Dictionary<String, FnObject> Parameters;
-        #endregion
+    internal FnVariable<Boolean> IsPreExecute;
 
-        #region Constructors
-        /// <summary>
-        /// Creates a new blank FnMethod
-        /// </summary>
-        /// <param name="argumentTypes">The datatype of each consecutive method argument</param>
-        public FnMethod()
-        {
-            Construct(null);
-        }
-        
-        /// <summary>
-        /// Creates a new blank FnMethod
-        /// </summary>
-        /// <param name="argumentTypes">The datatype of each consecutive method argument</param>
-        /// <param name="compileFlags">The list of compile flags the method should have</param>
-        public FnMethod(FnScriptResources.CompileFlags[] compileFlags)
-        {
-            Construct(compileFlags);
-        }
-        
-        /// <summary>
-        /// Constructs a blank FnMethod, called by the Constructors
-        /// </summary>
-        /// <param name="argumentTypes">The datatype of each consecutive method argument</param>
-        /// <param name="compileFlags">The list of compile flags the method should have</param>
-        private void Construct(FnScriptResources.CompileFlags[] compileFlags)
-        {
-            // Construct Argument Types
+    protected Dictionary<String, FnObject> Parameters;
+    #endregion
 
-            // Start by getting all the FnArgs defined for the method
-            ArgsInfo = this.GetType()
-                .GetRuntimeFields()                                                 // get fields
-                .Where(field => field.GetCustomAttribute(typeof(FnArg)) != null)    // that are also FnArgs
-                .ToArray<FieldInfo>();
-
-            // Now sift through the collected arguments and extract the types their FnObjects encompass
-            ArgumentTypes = new Type[ArgsInfo.Length];
-            for (int i = 0; i < ArgsInfo.Length; i++)
-            {
-                ArgumentTypes[i] = ArgsInfo[i].FieldType.GenericTypeArguments[0];
-            }
-
-            CompileFlags = (compileFlags != null) ? compileFlags : new FnScriptResources.CompileFlags[] { };
-        }
-        #endregion
-
-        public FnObject CreateNewBlankMethod()
-        {
-            return (FnObject)Activator.CreateInstance(this.GetType());
-        }
-
-        public override object GetValueAsObject()
-        {
-            return (Object)GetValue();
-        }
-
-        internal FnObject CreateNewMethod(List<FnObject> methodArguments, Dictionary<String, FnObject> parameters, FnVariable<Boolean> isPreExecute)
-        {
-            FnMethod<T> ReturnMethod = (CreateNewBlankMethod() as FnMethod<T>);
-            ReturnMethod.Populate(methodArguments, parameters, isPreExecute);
-
-            return ReturnMethod;
-        }
-
-        internal void Populate(List<FnObject> methodArguments, Dictionary<String, FnObject> parameters, FnVariable<Boolean> isPreExecute)
-        {
-            IsPreExecute = isPreExecute;
-            Parameters = parameters;
-
-            MethodArguments_CacheCheck = new FnObject[methodArguments.Count];
-
-            for (int i = 0; i < MethodArguments_CacheCheck.Length; i++)
-            {
-                MethodArguments_CacheCheck[i] = methodArguments[i];
-            }
-
-            //Now we push these into our named method arguments
-            for (int i = 0; i < methodArguments.Count; i++)
-            {
-                ArgsInfo[i].SetValue(this, methodArguments[i]);
-            }
-        }
-
-        internal override bool IsCachable()
-        {
-            // The method is only cachable if it is cachable
-            // And its arguments are cachable
-
-            // If the method is cachable
-            if (CompileFlags != null && CompileFlags.Contains(FnScriptResources.CompileFlags.DO_NOT_CACHE))
-            {
-                return false;
-            }
-
-            // Check each method argument (must be either a const or a cachable method)
-            if (MethodArguments_CacheCheck != null)
-            {
-                for (int i = 0; i < MethodArguments_CacheCheck.Length; i++)
-                {
-                    if (!MethodArguments_CacheCheck[i].IsCachable())
-                    {
-                        return false;
-                    }
-                }
-            }
-
-            return true;
-        }
-
-        internal override FnObject CheckAndCache()
-        {
-            // Optimize method arguments
-            for (int i = 0; i < ArgsInfo.Length; i++)
-            {
-                // Assign the optimized method arguments using reflection
-                ArgsInfo[i].SetValue(this, (ArgsInfo[i].GetValue(this) as FnObject).CheckAndCache());
-            }
-
-            // Optimize this node
-            FnObject returnValue;
-            if (IsCachable())
-            {
-                returnValue = new FnConstant<T>(GetValue());
-            }
-            else
-            {
-                returnValue = this;
-            }
-
-            ClearDataPostCache();
-
-            return returnValue;
-        }
-
-        /// <summary>
-        /// Clears any data this is no longer needed after an FnMethod has been cached
-        /// </summary>
-        internal void ClearDataPostCache()
-        {
-            MethodArguments_CacheCheck = null;
-            ArgumentTypes = null;
-            CompileFlags = null;
-            ArgsInfo = null;
-        }
+    #region Constructors
+    /// <summary>
+    /// Creates a new blank FnMethod
+    /// </summary>
+    /// <param name="argumentTypes">The datatype of each consecutive method argument</param>
+    public FnMethod() {
+      Construct(null);
     }
+
+    /// <summary>
+    /// Creates a new blank FnMethod
+    /// </summary>
+    /// <param name="argumentTypes">The datatype of each consecutive method argument</param>
+    /// <param name="compileFlags">The list of compile flags the method should have</param>
+    public FnMethod(FnScriptResources.CompileFlags[] compileFlags) {
+      Construct(compileFlags);
+    }
+
+    /// <summary>
+    /// Constructs a blank FnMethod, called by the Constructors
+    /// </summary>
+    /// <param name="argumentTypes">The datatype of each consecutive method argument</param>
+    /// <param name="compileFlags">The list of compile flags the method should have</param>
+    private void Construct(FnScriptResources.CompileFlags[] compileFlags) {
+      // Construct Argument Types
+
+      // Start by getting all the FnArgs defined for the method
+      ArgsInfo = this.GetType()
+          .GetRuntimeFields()                                                 // get fields
+          .Where(field => field.GetCustomAttribute(typeof(FnArg)) != null)    // that are also FnArgs
+          .ToArray<FieldInfo>();
+
+      // Now sift through the collected arguments and extract the types their FnObjects encompass
+      ArgumentTypes = new Type[ArgsInfo.Length];
+      for (int i = 0; i < ArgsInfo.Length; i++) {
+        ArgumentTypes[i] = ArgsInfo[i].FieldType.GenericTypeArguments[0];
+      }
+
+      CompileFlags = (compileFlags != null) ? compileFlags : new FnScriptResources.CompileFlags[] { };
+    }
+    #endregion
+
+    public FnObject CreateNewBlankMethod() {
+      return (FnObject)Activator.CreateInstance(this.GetType());
+    }
+
+    public override object GetValueAsObject() {
+      return (Object)GetValue();
+    }
+
+    internal FnObject CreateNewMethod(List<FnObject> methodArguments, Dictionary<String, FnObject> parameters, FnVariable<Boolean> isPreExecute) {
+      FnMethod<T> ReturnMethod = (CreateNewBlankMethod() as FnMethod<T>);
+      ReturnMethod.Populate(methodArguments, parameters, isPreExecute);
+
+      return ReturnMethod;
+    }
+
+    internal void Populate(List<FnObject> methodArguments, Dictionary<String, FnObject> parameters, FnVariable<Boolean> isPreExecute) {
+      IsPreExecute = isPreExecute;
+      Parameters = parameters;
+
+      MethodArguments_CacheCheck = new FnObject[methodArguments.Count];
+
+      for (int i = 0; i < MethodArguments_CacheCheck.Length; i++) {
+        MethodArguments_CacheCheck[i] = methodArguments[i];
+      }
+
+      //Now we push these into our named method arguments
+      for (int i = 0; i < methodArguments.Count; i++) {
+        ArgsInfo[i].SetValue(this, methodArguments[i]);
+      }
+    }
+
+    internal override bool IsCachable() {
+      // The method is only cachable if it is cachable
+      // And its arguments are cachable
+
+      // If the method is cachable
+      if (CompileFlags != null && CompileFlags.Contains(FnScriptResources.CompileFlags.DO_NOT_CACHE)) {
+        return false;
+      }
+
+      // Check each method argument (must be either a const or a cachable method)
+      if (MethodArguments_CacheCheck != null) {
+        for (int i = 0; i < MethodArguments_CacheCheck.Length; i++) {
+          if (!MethodArguments_CacheCheck[i].IsCachable()) {
+            return false;
+          }
+        }
+      }
+
+      return true;
+    }
+
+    internal override FnObject CheckAndCache() {
+      // Optimize method arguments
+      for (int i = 0; i < ArgsInfo.Length; i++) {
+        // Assign the optimized method arguments using reflection
+        ArgsInfo[i].SetValue(this, (ArgsInfo[i].GetValue(this) as FnObject).CheckAndCache());
+      }
+
+      // Optimize this node
+      FnObject returnValue;
+      if (IsCachable()) {
+        returnValue = new FnConstant<T>(GetValue());
+      } else {
+        returnValue = this;
+      }
+
+      ClearDataPostCache();
+
+      return returnValue;
+    }
+
+    /// <summary>
+    /// Clears any data this is no longer needed after an FnMethod has been cached
+    /// </summary>
+    internal void ClearDataPostCache() {
+      MethodArguments_CacheCheck = null;
+      ArgumentTypes = null;
+      CompileFlags = null;
+      ArgsInfo = null;
+    }
+  }
 }
--- Source/FunctionScript/FunctionScript/FnObject.cs
+++ Source/FunctionScript/FunctionScript/FnObject.cs
@@ -16,8 +16,17 @@ namespace FunctionScript {
     /// </summary>
     public abstract Object GetValueAsObject();
 
+    /// <summary>
+    /// Creates an <see cref="FnVariable"/> with the same wrapped type as this <see cref="FnObject"/>.
+    /// </summary>
     internal abstract FnObject CreateFnVariableWithSameType();
 
+    /// <summary>
+    /// Creates an <see cref="FnIf"/> object with the same wrapped type as this <see cref="FnObject"/>.
+    /// </summary>
+    /// <param name="condition">The conditional for the <see cref="FnIf"/>.</param>
+    /// <param name="trueArg">The execution tree to execute if <see cref="condition"/> returns true.</param>
+    /// <param name="falseArg">The execution tree to execute if <see cref="condition"/> returns false.</param>
     internal abstract FnObject CreateFnIfWithSameType(FnObject<bool> condition, FnObject trueArg, FnObject falseArg);
 
     /// <summary>
@@ -36,94 +45,38 @@ namespace FunctionScript {
     internal abstract FnObject CheckAndCache();
 
     /// <summary>
-    /// Determines if this FnObject is cachable
+    /// Determines if this <see cref="FnObject"/> is cachable.
     /// </summary>
     /// <returns></returns>
     internal abstract Boolean IsCachable();
   }
 
-
+  /// <summary>
+  /// Represent a FunctionScript language construct.
+  /// </summary>
   public abstract class FnObject<T> : FnObject {
     #region Abstract Methods
+    /// <summary>
+    /// Returns the contained value of this FnObject.
+    /// </summary>
+    /// <returns>The value.</returns>
     public abstract T GetValue();
     #endregion
 
-    #region Methods
-    /// <summary>
-    /// Returns the data type of the object the FnObject is wrapping
-    /// </summary>
-    /// <returns></returns>
+    #region Public Methods
     public override Type GetWrappedObjectType() {
       return typeof(T);
     }
+    #endregion
 
-    #region CreateFnObjectWithSameType methods
+    #region Internal Methods
     internal override FnObject CreateFnVariableWithSameType() {
       return new FnVariable<T>(default(T));
     }
-    /// <summary>
-    /// Createa a conditional FnObject with the same return type as the one the method is called from. Is useful as the arguments for a conditional FnObject should always be created before the conditional
-    /// </summary>
-    /// <param name="methodParameters"></param>
-    /// <returns></returns>
+
     internal override FnObject CreateFnIfWithSameType(FnObject<bool> condition, FnObject trueArg, FnObject falseArg) {
       return new FnIf<T>(condition, trueArg, falseArg);
     }
     #endregion
-
-    /// <summary>
-    /// Optimises the execution tree that branches out from this node.
-    /// Returns a new execution tree root node.
-    /// </summary>
-    //internal override FnObject CheckAndCache()
-    //{
-    //    Boolean cachable = true;
-
-    //    if (IsCachable())
-    //    {
-
-    //    }
-
-
-
-    //    Boolean cachable = true;
-
-    //    if (_MethodParameters != null)
-    //    {
-    //        for (int i = 0; i < _MethodParameters.Count; i++)
-    //        {
-    //            removedObjects.AddRange(_MethodParameters[i].CheckAndCache());
-
-    //            if ((_MethodParameters[i].CompileFlags != null && _MethodParameters[i].CompileFlags.Contains(FnScriptResources.CompileFlags.DO_NOT_CACHE)) || !_MethodParameters[i].IsVariable())
-    //            {
-    //                cachable = false;
-    //            }
-    //        }
-    //    }
-
-    //    if ((CompileFlags == null || !CompileFlags.Contains(FnScriptResources.CompileFlags.DO_NOT_CACHE)) && cachable == true)
-    //    {
-    //        GetValue();
-
-    //        EvalMethod = null;
-
-    //        //record the FnObjects which are being removed from the expression so they can be removed in the master list
-    //        if (_MethodParameters != null)
-    //        {
-    //            removedObjects.AddRange(_MethodParameters);
-    //        }
-
-    //        _MethodParameters = null;
-    //    }
-
-    //    return removedObjects;
-    //}
-    #endregion
-  }
-
-  [System.AttributeUsage(AttributeTargets.Field)]
-  public class FnArg : System.Attribute {
-    public FnArg() {
-    }
   }
 }
