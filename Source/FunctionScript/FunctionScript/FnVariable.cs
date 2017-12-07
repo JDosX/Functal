5fa835adcf1cef546c959f0fdf597eb8151313c4
Jayden Tilbrook <jdosx@live.com>
2017-12-07 23:50:30 +1100

    + Formatted FnVariable

--- Source/FunctionScript/FunctionScript/FnVariable.cs
+++ Source/FunctionScript/FunctionScript/FnVariable.cs
@@ -1,40 +1,33 @@
 using System;
 
-namespace FunctionScript
-{
-    public class FnVariable<T> : FnObject<T>
-    {
-        //FnVariable will also need a bit to link to the related FnMethod it gets
-        //its value from, which can be null if it isn't linked to anything
+namespace FunctionScript {
+  public class FnVariable<T> : FnObject<T> {
+    //FnVariable will also need a bit to link to the related FnMethod it gets
+    //its value from, which can be null if it isn't linked to anything
 
-        public T Value;
+    public T Value;
 
-        public FnVariable(T value)
-        {
-            Value = value;
-        }
-
-        internal override bool IsCachable()
-        {
-            return false;
-        }
-
-        /// <summary>
-        /// Returns an optimized version of this FnObject.
-        /// </summary>
-        internal override FnObject CheckAndCache()
-        {
-            return this;
-        }
-
-        public override T GetValue()
-        {
-            return Value;
-        }
-
-        public override object GetValueAsObject()
-        {
-            return (object)Value;
-        }
+    public FnVariable(T value) {
+      Value = value;
     }
+
+    internal override bool IsCachable() {
+      return false;
+    }
+
+    /// <summary>
+    /// Returns an optimized version of this FnObject.
+    /// </summary>
+    internal override FnObject CheckAndCache() {
+      return this;
+    }
+
+    public override T GetValue() {
+      return Value;
+    }
+
+    public override object GetValueAsObject() {
+      return (object)Value;
+    }
+  }
 }
