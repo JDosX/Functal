24683cf2c6a5154e31ec17392abee1c7387b34d9
Jayden Tilbrook <jdosx@live.com>
2018-03-09 18:32:35 +1100

    + started defining FunctionScript 1.5 compiler

--- Source/New_FunctionScript/FunctionScript/FunctionScript/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace FunctionScript
-{
-    public class Class1
-    {
-    }
-}
---
+++ Source/New_FunctionScript/FunctionScript/FunctionScript/Tokenizing/ConstToken.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Amadeus.Tokenizing;
+
+namespace FunctionScript.Tokenizing {
+  internal sealed class ConstToken : Token {
+    protected override IEnumerator<TokenState> Feed(char c) {
+      // First char must be [a-zA-Z_].
+      if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c == '_') {
+        yield return TokenState.InProgress;
+      } else {
+        yield return TokenState.Invalid;
+        yield break;
+      }
+
+      // Every subsequent character can be [a-zA-Z0-9_].
+      while (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c >= '0' && c <= '9' || c == '_') {
+        yield return TokenState.InProgress;
+      }
+
+      yield return TokenState.Done;
+      yield break;
+    }
+  }
+}
