759a391b2a1e691207ac533c0dd5087988e9fe91
Jayden Tilbrook <jdosx@live.com>
2018-03-28 09:12:22 +1100

    + added more tokenizing stuff

--- Source/New_FunctionScript/FunctionScript/FunctionScript/Tokenizing/ConstToken.cs
+++ Source/New_FunctionScript/FunctionScript/FunctionScript/Tokenizing/ConstToken.cs
@@ -1,25 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using Amadeus;
 using Amadeus.Tokenizing;
 
 namespace FunctionScript.Tokenizing {
   internal sealed class ConstToken : Token {
-    protected override IEnumerator<TokenState> Feed(char c) {
-      // First char must be [a-zA-Z_].
-      if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c == '_') {
-        yield return TokenState.InProgress;
-      } else {
-        yield return TokenState.Invalid;
+    internal sealed class Builder : Token.Builder {
+      protected override IEnumerator<BuildState> Feed(char? c) {
+        // First char must be a letter or underscore.
+        if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c == '_') {
+          yield return BuildState.InProgress;
+        } else {
+          yield return BuildState.Invalid;
+          yield break;
+        }
+
+        // Every subsequent character can be a letter, number or underscore.
+        while (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c >= '0' && c <= '9' || c == '_') {
+          yield return BuildState.InProgress;
+        }
+
+        yield return BuildState.Done;
         yield break;
       }
 
-      // Every subsequent character can be [a-zA-Z0-9_].
-      while (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c >= '0' && c <= '9' || c == '_') {
-        yield return TokenState.InProgress;
+      /// <inheritdoc/>
+      protected override Token CreateToken() {
+        return new ConstToken(Contents.ToString(), TokenStart, TokenEnd);
       }
-
-      yield return TokenState.Done;
-      yield break;
     }
+
+    /// <inheritdoc/>
+    public ConstToken(string content, StreamPosition tokenStart, StreamPosition tokenEnd)
+      : base(content, tokenStart, tokenEnd) {}
   }
 }
