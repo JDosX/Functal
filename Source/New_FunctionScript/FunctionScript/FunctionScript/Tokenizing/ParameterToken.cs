b3ffbee0edcacd26e834d83f6600ccc6de5eb9f7
Jayden Tilbrook <jdosx@live.com>
2018-03-28 09:12:22 +1100

    + added more tokenizing stuff

---
+++ Source/New_FunctionScript/FunctionScript/FunctionScript/Tokenizing/ParameterToken.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Amadeus.Tokenizing;
+
+namespace FunctionScript.Tokenizing {
+  public sealed class ParameterToken : Token {
+    public sealed class Builder : Builder {
+      
+    }
+  }
+}
